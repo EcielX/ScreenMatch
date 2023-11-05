4fdeb880c9fa58fe84a462d3facc396f2f244cc7
EcielX <112189551+EcielX@users.noreply.github.com>
2023-11-05 13:33:26 -0300

    Dados Episodio

---
+++ src/main/java/com/gmail/ecielxavier/screenmatch/Model/DadosEpisodio.java
@@ -0,0 +1,12 @@
+package com.gmail.ecielxavier.screenmatch.Model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Episode") Integer numero,
+                            @JsonAlias("imdbRating") String avaliação,
+                            @JsonAlias("Released") String dataLancamento) {
+
+}
--- src/main/java/com/gmail/ecielxavier/screenmatch/PrimeiroProjetoSpringSemWebApplication.java
+++ src/main/java/com/gmail/ecielxavier/screenmatch/PrimeiroProjetoSpringSemWebApplication.java
@@ -1,6 +1,7 @@
 package com.gmail.ecielxavier.screenmatch;
 
 import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import com.gmail.ecielxavier.screenmatch.Model.DadosEpisodio;
 import com.gmail.ecielxavier.screenmatch.Model.DadosSerie;
 import com.gmail.ecielxavier.screenmatch.Service.ConsumoApi;
 import com.gmail.ecielxavier.screenmatch.Service.ConverteDados;
@@ -18,12 +19,16 @@ public class PrimeiroProjetoSpringSemWebApplication implements CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi  = new ConsumoApi();
-		var json  = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=b7000c6");
+		var json  = consumoApi.obterDados("https://www.omdbapi.com/?t=the+office&apikey=b7000c6");
 		System.out.println(json);
 
 		ConverteDados converso = new ConverteDados();
 		DadosSerie dados = converso.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=the+office&season=1&episode=2&apikey=b7000c6");
+		DadosEpisodio dadosEpisodio = converso.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
 
 	}
 }
